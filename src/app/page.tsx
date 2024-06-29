0df7f0bd58198007c9540e9091e1fe5c896216eb
Sage9705 <edem22kumahor@gmail.com>
2024-06-29 18:51:16 +0100

    add about image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/herosection";
 import Navbar from "./components/navbar";
 import AboutSection from "./components/aboutsection";
