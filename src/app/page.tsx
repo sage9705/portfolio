29f05abafdc494f3d47d6df3cbd99487a531c038
Sage9705 <edem22kumahor@gmail.com>
2024-06-29 18:25:28 +0100

    add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/herosection";
 import Navbar from "./components/navbar";
+import AboutSection from "./components/aboutsection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] justify-between p-24 ">
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
