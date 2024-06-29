3a73c3f0366f0b4aba153d48bcf7cc31e6d20ded
Sage9705 <edem22kumahor@gmail.com>
2024-06-29 15:37:47 +0100

    add hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import HeroSection from "./components/herosection"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+      <HeroSection />
     </main>
   );
 }
