02c61fa8d6753b60e7f64899a05a21a02e233435
Sage9705 <edem22kumahor@gmail.com>
2024-06-29 18:59:16 +0100

    resolve deployment issues

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./navlink";
-// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./menuoverlay";
 import Image from "next/image";
 
