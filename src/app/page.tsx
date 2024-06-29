49068e3629add9ca4280789b611027176aef1e1a
Sage9705 <edem22kumahor@gmail.com>
2024-06-29 18:14:31 +0100

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import HeroSection from "./components/herosection"
+import HeroSection from "./components/herosection";
+import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] justify-between p-24 ">
+      <Navbar />
       <HeroSection />
     </main>
   );
